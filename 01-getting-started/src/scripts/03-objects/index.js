e2247e6875501a46d8368af90e94f0cb07d3fb54
Andrew Emil Bizon <h.c.draconis@Andrew-macbook.local>
2020-04-15 15:56:49 -0600

    April 15 16:00

--- 01-getting-started/src/scripts/03-objects/index.js
+++ 01-getting-started/src/scripts/03-objects/index.js
@@ -1,13 +1,22 @@
 import Account from './account.js'
 
+let acct1 = new Account();
+
+//"objects need to be instantiated outside of the listeners".....SteveB
+
+idCreateAcct.addEventListener("click", function() {
+    acct1 = new Account(idNew.value,Number(idOpenBal.value));
+    console.log(acct1.getSummary());
+    console.log(acct1.name,acct1.balance)
+    return acct1;
+});
+
+
+
 document.body.addEventListener("click", (e) => {
-    const acct1 = new Account(idNew.value,Number(idOpenBal.value));
     const cmd = e.target.textContent;
     console.log
-    if (cmd === 'Create') {
-        console.log(acct1.getSummary());
-        console.log(acct1.name,acct1.balance)
-    } else if (cmd === 'Deposit') {
+    if (cmd === 'Deposit') {
         acct1.deposit(Number(idDeposit.value));
     } else if (cmd === 'Withdrawl') {
         acct1.withdraw(Number(idWithdraw.value));
