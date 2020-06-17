7d88476e4f8824a6dabe352f0377f29a1a1d90d4
Andrew Bizon <andrew.bizon@icloud.com>
2020-06-17 16:44:08 -0600

    Near completed, need failsafe for an empty array

--- react-02group/src/components/BankAccount/AccountCard.js
+++ react-02group/src/components/BankAccount/AccountCard.js
@@ -6,6 +6,7 @@ class AccountCard extends Component {
         super(props)
         this.state = {
             depValue: 0,
+            withValue: 0
           }
     }
 
@@ -14,11 +15,25 @@ class AccountCard extends Component {
         this.setState({depValue: e.target.value})
     }
 
+    withdrawValue=(e)=>{
+        // console.log(e.target.value)
+        this.setState({withValue: e.target.value})
+    }
 
     depositInAcctCard=()=>{
         // console.log(this.state.depValue)
-        this.props.deposit(this.state.depValue,this.props.key1)
+        this.props.deposit(this.state.depValue, this.props.key1)
         this.setState({depValue: 0})
+    }
+
+    withdrawInAcctCard=()=>{
+        // console.log(this.state.depValue)
+        this.props.withdraw(this.state.withValue, this.props.key1)
+        this.setState({withValue: 0})
+    }
+
+    deleteAcctCard=()=>{
+        this.props.delete (this.props.key1)
     }
 
     render() {
@@ -27,11 +42,17 @@ class AccountCard extends Component {
         return (
         
         <div className="leftCard">
+
             <h2> {this.props.name}</h2>
             <h3> ${this.props.balance}</h3>
-            <input value={this.state.depValue} onChange={this.depositValue} type="number"></input><button onClick={this.depositInAcctCard}>Deposit</button><br/>
-            <input></input><button onClick={this.withdraw}>Withdraw</button><br/>
-            <button>Delete</button>
+            
+            <input value={this.state.depValue} onChange={this.depositValue} type="number"></input>
+            <button onClick={this.depositInAcctCard}>Deposit</button><br/>
+            
+            <input value={this.state.withValue} onChange={this.withdrawValue} type="number"></input>
+            <button onClick={this.withdrawInAcctCard}>Withdraw</button><br/>
+            
+            <button onClick={this.deleteAcctCard}>Delete</button>
 
             
         </div>
